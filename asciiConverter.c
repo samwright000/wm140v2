b9dfd5831333a7a6124e21b9f4b0a24833e9b19a
samwright000 <samualwright@outlook.com>
2021-10-18 12:49:19 +0100

    editing ascii

--- asciiConverter.c
+++ asciiConverter.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
+
 void main(void)
 {
 
 	int shiftValue;
 	char toShift[50];
-
+	int i;
 	printf("Please Enter Shift Value: ");
 	scanf("%i",&shiftValue);
 	
 	printf("Please Enter String To Be Encrptyed: ");
 	scanf("%s",toShift);
-	int i;
+	
+	for (i=0; i<strlen(toShift);i++){
+	
+	toShift[i]=toupper(toShift[i]);
+	}	
+	
 	for (i=0; i<strlen(toShift);++i){
 
 		char charToShift = toShift[i];
@@ -27,14 +34,8 @@ void main(void)
 		
 	}
 
-
-
 	//char ascii;
 	//printf("Please enter letter: ");
 	//scanf("%c",&ascii);
 	//printf("%i\n",ascii);
-	
-
-
-
 }
