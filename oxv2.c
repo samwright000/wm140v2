866e906c0afe52cb57f7bbab584863e398988fcb
laptop <samualwright@outlook.com>
2021-11-16 13:24:32 +0000

    updating

--- oxv2.c
+++ oxv2.c
@@ -36,7 +36,8 @@ char getPlayerInput(char board[9]){
 		if (cordLocation[1] == '1' || cordLocation[1] == '2' || cordLocation[1] == '3'){
 		
 			validInputPart2 = 1;
-		//	printf("\nDEBUG | cordLocation[1] is 1 2 or 3\n");}
+		//	printf("\nDEBUG | cordLocation[1] is 1 2 or 3\n");
+		}
 
 		
 		if (cordLocation[0] == 'a' && cordLocation[1] == '1'){location = '0';}
@@ -51,19 +52,27 @@ char getPlayerInput(char board[9]){
 	
 		intlocation = charToInt(location);
 
-		for (i=0; i<=9;i++){
+		for (i=0; i<=9;i++)
+		
+		{
 		//	printf("\nDEBUG | board[%d] = %c",i,board[intlocation]);
 
 		//	printf("\nDEBUG | before if\n");
-			printf("\n%c\n",board[intlocation]);
-			if (board[intlocation]=='-'){validInputPart3 = 1;i=10;printf("\nDEBUG | validinputpart3 = %d\n",validInputPart3);}
+			//printf("\n%c\n",board[intlocation]);
+			if (board[intlocation]=='-'){validInputPart3 = 1;i=10;
+			//	printf("\nDEBUG | validinputpart3 = %d\n",validInputPart3);
+			}
 		
 		//	printf("\nDEBUG | after if\n");
 		}
 
 		validInput = validInputPart1 + validInputPart2 + validInputPart3;
-		printf("\n\n\n%d\n\n\n",validInput);	
-		if (validInput == 3){printf("\nDEBUG | validInput = %d	part1 = %d part2 = %d part3 = %d \n",validInput,validInputPart1, validInputPart2, validInputPart3);break;}
+		//printf("\n\n\n%d\n\n\n",validInput);	
+		if (validInput == 3){
+			
+			//printf("\nDEBUG | validInput = %d	part1 = %d part2 = %d part3 = %d \n",validInput,validInputPart1, validInputPart2, validInputPart3);
+			
+			break;}
 	
 	}
 	
@@ -72,17 +81,18 @@ char getPlayerInput(char board[9]){
 }
 void drawBoard(char board[9], char player1Moves[9], char player2Moves[9]){
 
-	printf("  \nPLAYING BOARD\n");
-	printf("     a   b   c\n");
+	printf("\n\n   PLAYING BOARD\n");
+
 	printf("   +---+---+---+\n");
 	printf(" 1 | %c | %c | %c |\n",board[0],board[1],board[2]);
 	printf("   +---+---+---+\n");
 	printf(" 2 | %c | %c | %c |\n",board[3],board[4],board[5]);
 	printf("   +---+---+---+\n");
 	printf(" 3 | %c | %c | %c |\n",board[6],board[7],board[8]);
-	printf("   +---+---+---+\n");
-	printf("\nPLAYER 1 = X\n");
-	printf("PLAYER 2 = Y\n");
+	printf("   +---+---+---+\n");	
+	printf("     a   b   c\n");
+	printf("\nPLAYER 1 = x\n");
+	printf("PLAYER 2 = o\n");
 
 }
 
@@ -114,17 +124,16 @@ void printBoardArray(char array[9]){
 	printf("\n");
 	for (i=0; i<=sizeof(array);i++){
 	
-		printf("DEBUG | %c\n",array[i]);
+//		printf("DEBUG | %c\n",array[i]);
 	
 	}
 
 
 }
 
-int checkWhoWin(char player1Moves[9], char player2Moves[9]){
+int checkWhoWin(char playerMoves[9]){
 	
-	int i;
-	int ii;
+	int win = 0;
 
 	char across012[3] = "012";
 	char across345[3] = "345";
@@ -137,25 +146,36 @@ int checkWhoWin(char player1Moves[9], char player2Moves[9]){
 	char dag047[3] = "048";
 	char dag246[3] = "246";
 
+	if (playerMoves[0] != '-' && playerMoves[1] != '-' && playerMoves[2] != '-'){win = 1;}
+	if (playerMoves[3] != '-' && playerMoves[4] != '-' && playerMoves[5] != '-'){win = 1;}
+	if (playerMoves[6] != '-' && playerMoves[7] != '-' && playerMoves[8] != '-'){win = 1;}
+
+	if (playerMoves[0] != '-' && playerMoves[3] != '-' && playerMoves[6] != '-'){win = 1;}
+	if (playerMoves[1] != '-' && playerMoves[4] != '-' && playerMoves[7] != '-'){win = 1;}
+	if (playerMoves[2] != '-' && playerMoves[5] != '-' && playerMoves[8] != '-'){win = 1;}
+
+	if (playerMoves[0] != '-' && playerMoves[4] != '-' && playerMoves[8] != '-'){win = 1;}
+	if (playerMoves[2] != '-' && playerMoves[4] != '-' && playerMoves[6] != '-'){win = 1;}
+
+	return win; 	
+}
+
+int checkIfDraw(char board[9]){
+
+	int i;
+
 	for (i=0;i<=9;i++){
 	
-		for (ii=0;i<=2;i++){
+		if (board[i] == '-'){
 		
-			// Make who wins
-			// 
-			// - could do it like this and check for each condition are they in playermoves and just add
-			// 1 to a counter and if it is equal to 3 during a check then this means all it has all three numbers
-			// so could do it that a overall win is set to 1 
-			//  
-			//  also could do function that it just gets 1 array and chekcs rather than doing player1 then player 2
-			//  could have to variables for player 1 and player 2 instead of putting them both in the function as 
-			//  the code is going to be the same 
-
+		
+			return 0;
+		
 		}
 
 	}
 
-
+	return 1;
 
 }
 
@@ -169,66 +189,88 @@ void main(){
 	int playerInputInt;
 	int playerTurn = 1;
 	int i;
-
+	int checkPlayer1Win;
+	int checkPlayer2Win;
+	int isDraw;
+	system("clear");
 	while (1==1){
 		drawBoard(board,player1Moves,player2Moves);
 		playerInput = getPlayerInput(board);
 		playerInputInt = charToInt(playerInput);
-		printf("\nDEBUG | playerInput = %c\n",playerInput);
+		//printf("\nDEBUG | playerInput = %c\n",playerInput);
 	
 	
 		char toPlace;
 	
-		printf("\nDEBUG | before if playerTurn ==1\n");
+		//printf("\nDEBUG | before if playerTurn ==1\n");
 
 		if (playerTurn == 1){	
 			
 			toPlace = 'x';
+			player1Moves[playerInputInt] = toPlace;
+
+		//	printf("\nPlayer 1 Moves:\n\n");
 			
+
+			/*
 			for (i=0; i<=9; i++){
-				
-				printf("\nDEBUG | in for loop\n");
-				
-				if (player1Moves[i] == '-'){
-					printf("\nDEBUG | player1Moves[%d] = '-'\n",i);
-					player1Moves[i] = playerInput;
-					i=10;
-				}
+				printf("%c\n",player1Moves[i]);
+			}
+			*/
+			
+			checkPlayer1Win = checkWhoWin(player1Moves);
+
+			if (checkPlayer1Win == 1){
+			
+				printf("\n\n\nPlayer 1 Wins!!!\n\n\n");
+				break;
 			
 			}
-			
-			
-			printf("\nDEBUG | Out of loop\n");
+
 		}
 		
 		else{
 			
-			printf("\nDEBUG | playerturn not 1\n");
-			toPlace='y';
+			//printf("\nDEBUG | playerturn not 1\n");
+			toPlace='o';
+			player2Moves[playerInputInt] = toPlace;
 			
+			/*
+			printf("\nPlayer 2 Moves:\n\n");
 			for (i=0; i<=9; i++){
-				
-				printf("\nDEBUG | in loop for toplace y\n");
-				
-				if (player2Moves[i] == '-'){
-					
-					player2Moves[i] = playerInput;
-					
-					i=10;
-				
-				}
+				printf("%c\n",player2Moves[i]);
 			}
-		}
-		
-		printf("\n\nHELO\n\n");	
-		printf("\nDEBUG | toPlace = %c		playerInput = %c",toPlace);
-		board[playerInputInt] = toPlace;
+			*/
 
+			checkPlayer2Win = checkWhoWin(player2Moves);
+
+			if (checkPlayer2Win == 1){
+			
+				printf("\n\n\nPlayer 2 Wins!!!\n\n\n");
+				break;
+			}
+
+				
+		}
+
+		//printf("\nDEBUG | toPlace = %c		playerInput = %c",toPlace);
+		board[playerInputInt] = toPlace;
+		
+		isDraw = checkIfDraw(board);
+		//printf("\n\nDEBUG | isDraw = %d\n\n",isDraw); 
+		if (isDraw == 1){
+		
+			printf("\n\n\n No one wins :(. It was a draw! \n\n\n");
+			break;
+		
+		}
+
+		system("clear");
 
 		printf("\nPlayer %d has had there turn. It is now Player ",playerTurn);
 		playerTurn = playerChangeTurn(playerTurn);
 		printf("%d turn to play.",playerTurn);
-
 	}
+	
 }
 
